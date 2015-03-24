ccfb9822829aab8d410d103695c69c5be5116396
Chris Dopuch <cdopuch@appnexus.com>
2015-03-24 15:52:53 -0700

    Adds multi dir syncing including big sync

--- test/big-sync.mspec.js
+++ test/big-sync.mspec.js
@@ -6,9 +6,24 @@ var expect = require('chai').expect,
 
 var mockConfig = {
     excludes: ['one', 'two', 'three'],
-    sourceLocation: '/some/file/path',
+    sourceLocation: ['/some/file/path'],
     hostname: 'myCoolHost',
-    destinationLocation: '/some/where/out/there'
+    destinationLocation: ['/some/where/out/there']
+};
+
+var multiDirMockConfig = {
+    excludes: ['one', 'two', 'three'],
+    sourceLocation: [
+        '/some/file/path',
+        '/other/file/path',
+        '/yet/another/file/path'
+    ],
+    hostname: 'myCoolHost',
+    destinationLocation: [
+        '/some/destination/file/path',
+        '/some/other/destination/file/path',
+        '/all/the/destination/file/paths'
+    ]
 };
 
 var rsyncSpies = (function() {
@@ -24,9 +39,13 @@ var rsyncSpies = (function() {
     return new BuildSpies();
 })();
 
-function RsyncMockConstructor() {
-    return rsyncSpies;
-}
+var RsyncMockConstructorSpyObject = { 
+    RsyncMockConstructor: function() {
+        return rsyncSpies;
+    }
+};
+
+var rsyncConstructorSpy = sinon.spy(RsyncMockConstructorSpyObject, 'RsyncMockConstructor');
 
 describe('bigSync', function() {
 
@@ -59,13 +78,13 @@ describe('bigSync', function() {
     describe('when executed', function() {
 
         beforeEach(function(done) {
-            bigSync.__set__('Rsync', RsyncMockConstructor);
+            bigSync.__set__('Rsync', RsyncMockConstructorSpyObject.RsyncMockConstructor);
             bigSync.__set__('config', mockConfig);
             bigSync(done);
         });
 
         afterEach(function() {
-            testUtils.resetSpies(rsyncSpies);
+            // testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
         });
 
         it('should set the `shell` property to `ssh`', function() {
@@ -81,12 +100,29 @@ describe('bigSync', function() {
         });
 
         it('should set the `source` property to match the on one in the config', function() {
-            expect(mockConfig.sourceLocation).to.equal(rsyncSpies.source.getCall(0).args[0]);
+            expect(mockConfig.sourceLocation[0]).to.equal(rsyncSpies.source.getCall(0).args[0]);
         });
 
         it('should set the `destination` property to match the on one in the config', function() {
             var destination = mockConfig.hostname + ':' + mockConfig.destinationLocation;
             expect(destination).to.equal(rsyncSpies.destination.getCall(0).args[0]);
+        });
+    });
+
+    describe('with multiple directories to sync', function(){
+        beforeEach(function(done) {
+            rsyncConstructorSpy.reset();
+            bigSync.__set__('Rsync', RsyncMockConstructorSpyObject.RsyncMockConstructor);
+            bigSync.__set__('config', multiDirMockConfig);
+            bigSync(done);
+        });
+
+        afterEach(function() {
+            // testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
+        });
+
+        it('should call rsync a number of times equal to the length of config.sourceLocation', function() {
+            expect(rsyncConstructorSpy.callCount).to.equal(multiDirMockConfig.sourceLocation.length);
         });
     });
 });
