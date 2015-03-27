a9448e88ddac62cfca1c7afea5d78f2f48f3e4bb
Chris Dopuch <cdopuch@appnexus.com>
2015-03-27 10:58:33 -0700

    adds more tests to cover big-sync

--- test/big-sync.mspec.js
+++ test/big-sync.mspec.js
@@ -4,27 +4,20 @@ var expect = require('chai').expect,
     bigSync = rewire('../lib/big-sync'),
     testUtils = require('./utils');
 
-var mockConfig = {
-    excludes: ['one', 'two', 'three'],
-    sourceLocation: ['/some/file/path'],
-    hostname: 'myCoolHost',
-    destinationLocation: ['/some/where/out/there']
-};
+var mockConfig = testUtils.makeMockConfig();
 
-var multiDirMockConfig = {
-    excludes: ['one', 'two', 'three'],
+var multiDirMockConfig = testUtils.makeMockConfig({
     sourceLocation: [
         '/some/file/path',
         '/other/file/path',
         '/yet/another/file/path'
     ],
-    hostname: 'myCoolHost',
     destinationLocation: [
         '/some/destination/file/path',
         '/some/other/destination/file/path',
         '/all/the/destination/file/paths'
     ]
-};
+});
 
 var rsyncSpies = (function() {
     function BuildSpies() {}
@@ -84,7 +77,8 @@ describe('bigSync', function() {
         });
 
         afterEach(function() {
-            // testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
+            testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
+            rsyncConstructorSpy.reset();
         });
 
         it('should set the `shell` property to `ssh`', function() {
@@ -111,18 +105,45 @@ describe('bigSync', function() {
 
     describe('with multiple directories to sync', function(){
         beforeEach(function(done) {
-            rsyncConstructorSpy.reset();
             bigSync.__set__('Rsync', RsyncMockConstructorSpyObject.RsyncMockConstructor);
             bigSync.__set__('config', multiDirMockConfig);
             bigSync(done);
         });
 
         afterEach(function() {
-            // testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
+            testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
+            rsyncConstructorSpy.reset();
         });
 
         it('should call rsync a number of times equal to the length of config.sourceLocation', function() {
             expect(rsyncConstructorSpy.callCount).to.equal(multiDirMockConfig.sourceLocation.length);
+        });
+    });
+
+    describe('with incorrectly formatted config source/destination locations', function() {
+        beforeEach(function() {
+            bigSync.__set__('Rsync', RsyncMockConstructorSpyObject.RsyncMockConstructor);
+        });
+
+        afterEach(function() {
+            testUtils.resetSpies(rsyncSpies); // TODO: this doesn't actually reset these spies, needs to be fixed
+            rsyncConstructorSpy.reset();
+        });
+
+        it('should throw an exception when source/destination locations are not arrays or strings', function() {
+            bigSync.__set__('config', testUtils.makeMockConfig({
+                sourceLocation: null,
+                destinationLocation: null
+            }));
+            expect(bigSync).to.throw(Error);
+        });
+
+        it('should throw an exception when source/destination locations are not the same length', function() {
+            bigSync.__set__('config', testUtils.makeMockConfig({
+                sourceLocation: ['1', '2', '3'],
+                destinationLocation: ['1', '2']
+            }));
+            expect(bigSync).to.throw(Error);
         });
     });
 });
--- test/utils.js
+++ test/utils.js
@@ -3,5 +3,14 @@ module.exports = {
         Object.keys(spyObject).forEach(function(prop) {
             spyObject[prop].reset();
         });
+    },
+    makeMockConfig: function(params) {
+    	params = params || {};
+    	return {
+    		excludes: params.excludes || ['one', 'two', 'three'],
+		    sourceLocation: params.sourceLocation || ['/some/file/path'],
+		    hostname: params.hostname || 'myCoolHost',
+		    destinationLocation: params.destinationLocation || ['/some/where/out/there']
+    	};
     }
 };
