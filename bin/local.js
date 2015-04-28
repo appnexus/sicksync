5144d06a17352fc3c3c4c10dd67737a180e62968
Joel Griffith <jgriffith@appnexus.com>
2015-04-28 09:42:46 -0700

    Pausing file watch on BigSync, updating threshold on bigsync fallover

--- bin/local.js
+++ bin/local.js
@@ -6,47 +6,47 @@
  *  Entry point into the client portion of sicksync
  */
 var fs = require('fs'),
-    watcher = require('chokidar'),
+    chokidar = require('chokidar'),
     path = require('path'),
     util = require('../lib/util'),
     WebSocketClient = require('../lib/ws-client'),
     bigSync = require('../lib/big-sync'),
     config = util.getConfig(),
     ignored = config.excludes,
-    isPaused = false,
-    devbox = null;
+    devbox = null,
+    watcher = null;
 
 require('colors');
 
-var NUM_FILES_FOR_LARGE_SEND = 10;
-var FILE_CHANGE_COOLDOWN_TIME = 10;
+var NUM_FILES_FOR_LARGE_SEND = 3;
+var FILE_CHANGE_COOLDOWN_TIME = 20;
 
 var rebouncedFileChange = util.rebounce(onFileChange, onBigTransfer, NUM_FILES_FOR_LARGE_SEND, FILE_CHANGE_COOLDOWN_TIME);
 
 function onBigTransfer() {
     if (config.debug) console.log('[local] Sending large change');
-
+    
+    stopFileWatch();
     bigSync(onBigTransferDone);
-    isPaused = true;
 }
 
 function onBigTransferDone() {
     if (config.debug) {
         console.log('['+ config.hostname +'] Received large change');
+        startFileWatch();
     }
-    isPaused = false;
 }
 
 function filterAndRebounce(evt, filepath) {
     var relativePath = filepath.replace(config.sourceLocation, '');
 
-    if (util.isExcluded(relativePath, ignored) || isPaused) return false;
+    if (util.isExcluded(relativePath, ignored)) return false;
 
     rebouncedFileChange(evt, filepath);
 }
 
 function onFileChange(evt, filepath) {
-    if (util.isExcluded(filepath, ignored) || isPaused) return false;
+    if (util.isExcluded(filepath, ignored))  return false;
     var fileContents = null;
     var localPath = filepath.replace(config.sourceLocation, '');
 
@@ -68,12 +68,16 @@ function onFileChange(evt, filepath) {
 }
 
 function startFileWatch() {
-    watcher.watch(config.sourceLocation, {
+    watcher = chokidar.watch(config.sourceLocation, {
         ignored: ignored,
         persistent: true,
         followSymlinks: false,
         ignoreInitial: true
     }).on('all', filterAndRebounce);
+}
+
+function stopFileWatch() {
+    watcher.close();
 }
 
 function onAuthorized() {
