f0abbda6f40d4d7f7e76dd704e84815ec3e2b748
Joel Griffith <jgriffith@244.jgriffith.user.lax1>
2015-07-16 19:52:02 +0000

    Remote command fixes

--- commands/remote.js
+++ commands/remote.js
@@ -2,7 +2,7 @@ var remote = require('../lib/remote');
 
 module.exports = function sicksyncRemoteCommand(program) {
 	program
-        .command('remote [options]')
+        .command('remote')
         .option('-s, --secret <secret>', 'A secret used to block unkown subscribers (should match with config.secret).')
         .option('-p, --port <port>', 'The port in which to listen for incoming sync messages.')
         .option('-d, --debug', 'Show debug messages')
