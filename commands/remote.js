3b0481a8ed0860b8e0a725365905fcc7c4793b2f
Joel Griffith <jgriffith@244.jgriffith.user.lax1>
2015-07-16 19:41:39 +0000

    Server updates

--- commands/remote.js
+++ commands/remote.js
@@ -3,7 +3,6 @@ var remote = require('../lib/remote');
 module.exports = function sicksyncRemoteCommand(program) {
 	program
         .command('remote [options]')
-        .description('Starts the remote portion of sicksync.')
         .option('-s, --secret <secret>', 'A secret used to block unkown subscribers (should match with config.secret).')
         .option('-p, --port <port>', 'The port in which to listen for incoming sync messages.')
         .option('-d, --debug', 'Show debug messages')
