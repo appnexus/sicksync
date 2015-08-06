09bfd64eda790c2814a27023b6e901e8fad4b25b
Joel Griffith <jgriffith@appnexus.com>
2015-08-05 20:17:34 -0700

    Almost there...

--- commands/remote.js
+++ commands/remote.js
@@ -6,5 +6,6 @@ module.exports = function sicksyncRemoteCommand(program) {
         .option('-s, --secret <secret>', 'A secret used to block unkown subscribers (should match with config.secret).')
         .option('-p, --port <port>', 'The port in which to listen for incoming sync messages.')
         .option('-d, --debug', 'Show debug messages')
+        .option('-e, --encrypt', 'Enable encryption on messages')
         .action(remote);
 };
