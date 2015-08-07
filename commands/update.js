e030914baffb2c64ec2ea7a1f17cb84058e4833d
Joel Griffith <jgriffith@appnexus.com>
2015-08-07 10:36:59 -0700

    Fixing updates, adding update helper

--- commands/update.js
+++ commands/update.js
@@ -1,8 +1,17 @@
-var update = require('../lib/update').update;
+var update = require('../lib/update');
 
 module.exports = function sicksyncOnceCommand(program) {
     program
         .command('update')
         .description('Updates sicksync on both your local and remote machines')
-        .action(update);
+        .option('-c, --check, Checks to see if a new version is available')
+        .action(function(cmd) {
+            if (cmd.check) {
+                if (!update.notifyUpdates()) {
+                    console.log('Up to date!');
+                }
+                return;
+            }
+            return update.update();
+        });
 };
