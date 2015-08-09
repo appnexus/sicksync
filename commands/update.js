7961d189a280eddb4f09719843c24031cdd4f68c
Joel Griffith <jgriffith@appnexus.com>
2015-08-08 21:18:07 -0700

    Adding in constants, cleanup in updates file`

--- commands/update.js
+++ commands/update.js
@@ -1,16 +1,18 @@
-var update = require('../lib/update');
+var update = require('../lib/update'),
+    package = require('../package.json');
 
 module.exports = function sicksyncOnceCommand(program) {
     program
         .command('update')
         .description('Updates sicksync on both your local and remote machines')
-        .option('-c, --check, Checks to see if a new version is available')
+        .option('-c, --check, Checks to see the latest version of sicksync')
         .action(function(cmd) {
             if (cmd.check) {
-                if (!update.notifyUpdates()) {
-                    console.log('Up to date!');
-                }
-                return;
+                return update.getLatestVersion(function(err, version) {
+                    if (err) return;
+                    console.log('Latest Version:', version);
+                    console.log('Current Version:', package.version);
+                });
             }
             return update.update();
         });
---
+++ conf/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    UPDATE_CMD: 'npm i -g sicksync',
+    UPDATE_INTERVAL: 1000 * 60 * 60 * 24, // Check once a day
+};
