9e12d90ada0664b3fa43356aca19c937f73cba7f
Joel Griffith <jgriffith@appnexus.com>
2015-08-08 21:35:00 -0700

    More conf updates, clearing up rebounce

--- conf/constants.js
+++ conf/constants.js
@@ -1,4 +1,10 @@
 module.exports = {
     UPDATE_CMD: 'npm i -g sicksync',
-    UPDATE_INTERVAL: 1000 * 60 * 60 * 24, // Check once a day
+    UPDATE_INTERVAL: 1000 * 60 * 60 * 24,
+    NUM_FILES_FOR_RSYNC: 10,
+    FILE_CHANGE_COOLDOWN_MS: 10,
+    CRYPT_ALGO: 'aes-128-ctr',
+    CONFIG_FILE: 'config.json',
+    UPDATE_FILE: 'update.json',
+    SICKSYNC_DIR: '.sicksync'
 };
