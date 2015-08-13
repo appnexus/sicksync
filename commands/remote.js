0e07a16960e65e88069c3bfc2ff32f90bf303058
Joel Griffith <jgriffith@appnexus.com>
2015-08-13 09:40:18 -0700

    Mostly there with advanced logging

--- commands/remote.js
+++ commands/remote.js
@@ -4,6 +4,7 @@ module.exports = function sicksyncRemoteCommand(program) {
     program
         .command('remote')
         .description('Starts the remote process of sicksync. Must be ran on the remote machine.')
+        .option('-H, --hostname <hostname>', 'The name of the host as it appears in your project config.')
         .option('-s, --secret <secret>', 'A secret used to only allow known subscribers (should match with your config).')
         .option('-p, --port <port>', 'The port in which to listen for incoming sync messages (should match with your config).', parseInt)
         .option('-e, --encrypt', 'Enable encryption on messages (should match with your config)')
