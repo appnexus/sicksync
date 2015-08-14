dd2d226e0304fee752250bc44e59596590cefbb2
Joel Griffith <jgriffith@appnexus.com>
2015-08-14 09:56:34 -0700

    Adding in a text json file for message consolidation

--- commands/once.js
+++ commands/once.js
@@ -1,16 +1,26 @@
-var bigSync = require('../lib/big-sync'),
+var _ = require('lodash'),
+    bigSync = require('../lib/big-sync'),
     util = require('../lib/util');
 
-module.exports = function sicksyncOnceCommand(program/*, config */) {
+module.exports = function sicksyncOnceCommand(program, config) {
     program
         .command('once <projects...>')
         .description('Runs a one-time sync')
         .option('-n, --dry-run', 'Shows information on what files will be sent without sending them')
-        .action(function(projects, cmd) {
-            // TODO
-            util.log('Starting one-time sync');
-            bigSync({ dry: cmd.dryRun }, function() {
-                util.log('Sync complete!');
+        .action(function(projects, opts) {
+            _.each(projects, function(project) {
+                if (_.isEmpty(config.projects[project])) {
+                    return console.log('Could\'t sync project since it was not found in your config:', project);
+                }
+                var projectConf = config.projects[project];
+                var log = util.generateLog(projectConf.project, projectConf.hostname);
+                
+                log('Starting one-time sync');
+
+                bigSync(projectConf, {
+                    dry: opts.dryRun,
+                    debug: config.debug
+                }, _.partial(log, 'Complete:'));
             });
         });
 };
---
+++ conf/text.js
@@ -0,0 +1,14 @@
+module.exports = {
+    PROJECT_NOT_FOUND: 'We couldn\'t find this project in your config:',
+    SYNC_ON_ONCE: 'Starting a one-time sync',
+    SYNC_ON_ONCE_DONE: 'One-time sync complete!',
+    SYNC_ON_CONNECT: 'Connected',
+    SYNC_ON_RECONNECT: 'Reconnecting',
+    SYNC_ON_DISCONNECT: 'Lost connection',
+    SYNC_ON_REMOTE_LOST: 'Couldn\'t start sicksync process',
+    SYNC_ON_LARGE_CHANGE: 'Sending a large change',
+    SYNC_ON_LARGE_CHANGE_DONE: 'Large change sent!',
+    SERVER_ON_UNAUTHORIZED: 'Unauthorized connection, shutting down',
+    SERVER_ON_CONNECTION_CLOSED: 'Connection closed, shutting down',
+    SERVER_ON_READY: 'Sicksync ready'
+};
