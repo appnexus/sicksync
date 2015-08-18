7ad3026fff5f6f5653968b4f2605e5e270ddc77e
Joel Griffith <jgriffith@appnexus.com>
2015-08-17 18:52:29 -0700

    Fixing tests and lib (thanks test)

--- test/local/remote-helper.mspec.js
+++ test/local/remote-helper.mspec.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect,
+    _ = require('lodash'),
     sinon = require('sinon'),
     rewire = require('rewire'),
+    text = require('../../conf/text'),
     Helper = rewire('../../lib/local/remote-helper');
 
 var sshMock = {
@@ -14,15 +16,21 @@ var sshMock = {
 
 var utilMock = {
     getConfig: sinon.spy(),
-    shellIntoRemote: sinon.stub().returns(sshMock)
+    shellIntoRemote: sinon.stub().returns(sshMock),
+    resetAll: function () {
+        _.forIn(utilMock, function(method, key) {
+            if (key !== 'resetAll') method.reset();
+        });
+    }
 };
 
-var configMock = {
+var params = {
+    username: 'joel',
+    hostname: 'myhost',
     secret: 'dirty-little-secret',
     websocketPort: 1234,
-    prefersEncrypted: false,
-    debug: false,
-    destinationLocation: 'my-dev-box'
+    prefersEncrypted: true,
+    debug: true
 };
 
 function triggerStdout(message) {
@@ -35,20 +43,20 @@ describe('remote-helper', function () {
 
     beforeEach(function () {
         Helper.__set__('util', utilMock);
-        helper = new Helper(configMock);
+        helper = new Helper(params);
     });
 
     afterEach(function() {
         Helper.__set__('util', utilCache);
         sshMock.stdout.on.reset();
         sshMock.stdin.write.reset();
-        testUtils.resetSpies(utilMock);
+        utilMock.resetAll();
     });
 
     describe('#start', function () {
         it('should shell into the remote box', function() {
             helper.start();
-            expect(utilMock.shellIntoRemote.called).to.be.true;
+            expect(utilMock.shellIntoRemote.lastCall.args[0]).to.equal(params.username + '@' + params.hostname);
         });
 
         it('should listen to data coming in from the remote host', function() {
@@ -60,17 +68,29 @@ describe('remote-helper', function () {
         it('should emit a ready flag once the server starts up', function(done) {
             helper.start();
             helper.once('ready', done);
-            triggerStdout('ready');
+            triggerStdout(text.SERVER_ON_READY);
         });
 
-        it('should emit a message event if the message contains the devbox name', function(done) {
-            var message = configMock.destinationLocation + ' some message!';
+        it('should emit a message event if the message contains the secret', function(done) {
+            var message = 'some message!';
+            var event = params.secret + ' ' + message;
             helper.start();
             helper.once('message', function(data) {
-                expect(data).to.equal(message);
+                expect(data).to.contain(message);
                 done();
             });
-            triggerStdout(message);
+            triggerStdout(event);
+        });
+
+        it('should emit an error event when the command isn\'t found', function(done) {
+            var errorMessage = 'command not found';
+            helper.start();
+            helper.once('error', function(data) {
+                expect(data).to.contain(errorMessage);
+                done();
+            });
+            triggerStdout(errorMessage);
+
         });
 
         describe('starting the sicksync process', function () {
@@ -80,17 +100,17 @@ describe('remote-helper', function () {
                     expect(sshMock.stdin.write.called).to.be.true;
                     done();
                 });
-                triggerStdout('ready');
+                triggerStdout(text.SERVER_ON_READY);
             });
 
             it('should only happen once', function(done) {
-                var message = configMock.destinationLocation + ' some message!';
+                var message = params.secret + ' some message!';
                 helper.start();
                 helper.once('message', function() {
                     expect(sshMock.stdin.write.calledOnce).to.be.true;
                     done();
                 });
-                triggerStdout('ready');
+                triggerStdout(text.SERVER_ON_READY);
                 triggerStdout(message);
             });
 
@@ -102,20 +122,24 @@ describe('remote-helper', function () {
                     expect(remoteCmd).to.contain('-p 1234');
                     done();
                 });
-                triggerStdout('ready');
+                triggerStdout(text.SERVER_ON_READY);
             });
 
             it('should exclude the `debug` and `encrypt` flags if not present in config', function(done) {
-                helper._config.debug = true;
-                helper._config.prefersEncrypted = true;
+                var noDebugOrExcludeConfig = _.clone(params);
+                
+                noDebugOrExcludeConfig.debug = false;
+                noDebugOrExcludeConfig.prefersEncrypted = false;
+                
+                helper = new Helper(noDebugOrExcludeConfig);
                 helper.start();
                 helper.once('ready', function() {
                     var remoteCmd = sshMock.stdin.write.lastCall.args[0];
-                    expect(remoteCmd).to.contain('-e');
-                    expect(remoteCmd).to.contain('-d');
+                    expect(remoteCmd).to.not.contain('-e');
+                    expect(remoteCmd).to.not.contain('-d');
                     done();
                 });
-                triggerStdout('ready');
+                triggerStdout(text.SERVER_ON_READY);
             });
         });
     });
