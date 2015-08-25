ba111bed4e48cf6580200f2c686288446eaa9e01
Joel Griffith <jgriffith@appnexus.com>
2015-08-25 13:21:44 -0700

    Ws-client is now es6ified

--- test/local/ws-client.mspec.js
+++ test/local/ws-client.mspec.js
@@ -1,8 +1,12 @@
 var expect = require('chai').expect,
     _ = require('lodash'),
-    rewire = require('rewire'),
-    sinon = require('sinon'),
-    Client = rewire('../../src/local/ws-client');
+    remoteHelperStub = require('../stubs/remote-helper'),
+    wsStub = require('../stubs/ws'),
+    proxyquire = require('proxyquire'),
+    Client = proxyquire('../../src/local/ws-client', {
+        './remote-helper': remoteHelperStub,
+        'ws': wsStub
+    });
 
 var params = {
     secret: 'keepitsafe',
@@ -13,28 +17,6 @@ var params = {
     username: 'joel'
 };
 
-var wsMock = {
-    on: sinon.stub(),
-    send: sinon.stub(),
-    resetAll: function() {
-        _.forIn(wsMock, function(method, key) {
-            if (key !== 'resetAll') method.reset();
-        });
-    }
-};
-
-var WsMock = sinon.stub().returns(wsMock);
-
-var devboxMock = {
-    start: sinon.spy(),
-    on: sinon.spy()
-};
-
-var DevboxMock = sinon.stub().returns(devboxMock);
-
-Client.__set__('WebSocket', WsMock);
-Client.__set__('RemoteHelper', DevboxMock);
-
 describe('ws-client', function() {
     var ws = null;
 
@@ -43,29 +25,28 @@ describe('ws-client', function() {
     });
 
     afterEach(function() {
-        devboxMock.on.reset();
-        devboxMock.start.reset();
-        wsMock.resetAll();
+        remoteHelperStub.resetAll();
+        wsStub.resetAll();
     });
 
     describe('connection', function() {
         it('should register callbacks via the `on` method', function() {
-            expect(wsMock.on.called).to.be.true;
+            expect(wsStub._api.on.called).to.be.true;
         });
 
         it('should register a callback for the `open` message', function() {
-            expect(wsMock.on.getCall(0).args[0]).to.equal('open');
-            expect(wsMock.on.getCall(0).args[1]).to.be.a('function');
+            expect(wsStub._api.on.getCall(0).args[0]).to.equal('open');
+            expect(wsStub._api.on.getCall(0).args[1]).to.be.a('function');
         });
 
         it('should register a callback for the `close` message', function() {
-            expect(wsMock.on.getCall(1).args[0]).to.equal('close');
-            expect(wsMock.on.getCall(1).args[1]).to.be.a('function');
+            expect(wsStub._api.on.getCall(1).args[0]).to.equal('close');
+            expect(wsStub._api.on.getCall(1).args[1]).to.be.a('function');
         });
 
         it('should register a callback for the `error` message', function() {
-            expect(wsMock.on.getCall(2).args[0]).to.equal('error');
-            expect(wsMock.on.getCall(2).args[1]).to.be.a('function');
+            expect(wsStub._api.on.getCall(2).args[0]).to.equal('error');
+            expect(wsStub._api.on.getCall(2).args[1]).to.be.a('function');
         });
     });
 
@@ -73,34 +54,34 @@ describe('ws-client', function() {
         it('should attach the secret to the passed in object', function() {
             ws.send({some: 'object'});
 
-            expect(JSON.parse(wsMock.send.lastCall.args[0]).secret).to.not.be.a('undefined');
+            expect(JSON.parse(wsStub._api.send.lastCall.args[0]).secret).to.not.be.a('undefined');
         });
     });
 
     describe('onOpen', function() {
         beforeEach(function() {
             // Trigger `open`
-            wsMock.on.getCall(0).args[1]();
+            wsStub._api.on.getCall(0).args[1]();
         });
 
         it('should emit a `ready` event', function(done) {
             ws.on('ready', done);
-            wsMock.on.getCall(0).args[1]();
+            wsStub._api.on.getCall(0).args[1]();
         });
     });
 
     describe('onClose', function() {
         beforeEach(function() {
             // Trigger `close`
-            wsMock.on.getCall(1).args[1]();
+            wsStub._api.on.getCall(1).args[1]();
         });
 
         it('should attempt to re-awaken the devbox if `retryOnDisconnect` is true', function() {
-            expect(devboxMock.start.called).to.be.true;
+            expect(remoteHelperStub._api.start.called).to.be.true;
         });
         
         it('should emit a `disconnected` event when `retryOnDisconnect` is false', function(done) {
-            wsMock.on.reset();
+            wsStub._api.on.reset();
             var noRetryParams = _.clone(params);
             noRetryParams.retryOnDisconnect = false;
 
@@ -108,35 +89,35 @@ describe('ws-client', function() {
             wsNoRetry.once('disconnected', done);
 
             // Trigger `close`
-            wsMock.on.getCall(1).args[1]();
+            wsStub._api.on.getCall(1).args[1]();
         });
     });
 
     describe('onError', function() {
         beforeEach(function() {
             // Trigger `error`
-            wsMock.on.getCall(2).args[1]();
+            wsStub._api.on.getCall(2).args[1]();
         });
 
         it('should emit an `reconnecting` event', function(done) {
             ws.on('reconnecting', done);
 
             // Trigger `error`
-            wsMock.on.getCall(2).args[1]();
+            wsStub._api.on.getCall(2).args[1]();
         });
 
         it('should start the remote devbox', function() {
-            expect(devboxMock.start.called).to.be.true;
+            expect(remoteHelperStub._api.start.called).to.be.true;
         });
 
         it('should listen for the `ready` message from the devbox', function() {
-            expect(devboxMock.on.getCall(0).args[0]).to.equal('ready');
-            expect(devboxMock.on.getCall(0).args[1]).to.be.a('function');
+            expect(remoteHelperStub._api.on.getCall(0).args[0]).to.equal('ready');
+            expect(remoteHelperStub._api.on.getCall(0).args[1]).to.be.a('function');
         });
 
         it('should listen for the `message` message from the devbox', function() {
-            expect(devboxMock.on.getCall(1).args[0]).to.equal('message');
-            expect(devboxMock.on.getCall(1).args[1]).to.be.a('function');
+            expect(remoteHelperStub._api.on.getCall(1).args[0]).to.equal('message');
+            expect(remoteHelperStub._api.on.getCall(1).args[1]).to.be.a('function');
         });
     });
 });
