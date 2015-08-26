243ac1358f6f8b37a6cd1ae1c726ea974b2b34bd
Joel Griffith <jgriffith@appnexus.com>
2015-08-26 15:51:17 -0700

    More es6ifing, breaking update out into functions

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,5 +1,5 @@
 import { partial } from 'lodash';
-import update from '../update';
+import { update } from '../update';
 
 function sicksyncUpdateCommand(program, config) {
     program
@@ -7,7 +7,7 @@ function sicksyncUpdateCommand(program, config) {
         .description('Updates sicksync on both your local and remote machine(s)')
         .option('-c, --check', 'Checks to see the latest version of sicksync')
         .option('-m, --migrate-config', 'Updates your config for the most recent version for sicksync')
-        .action(partial(update.update.bind(update), config));
+        .action(partial(update, config));
 };
 
 export default sicksyncUpdateCommand;
--- src/update.js
+++ src/update.js
@@ -17,111 +17,128 @@ let _ = require('lodash'),
             version: packageJson.version
         };
 
-module.exports = {
-    getLatestVersion: _.partial(_.ary(latestVersion, 2), 'sicksync'),
-    updateRemote (project) {
-        let ssh = util.shellIntoRemote(project.username + '@' + project.hostname);
 
-        let updateRemoteOnce = _.once(function(stdin){
-            stdin.write(constants.UPDATE_CMD + '\n');
+let getLatestVersion = _.partial(_.ary(latestVersion, 2), 'sicksync');
+
+function updateRemote (project) {
+    let ssh = util.shellIntoRemote(project.username + '@' + project.hostname);
+
+    let updateRemoteOnce = _.once(function(stdin){
+        stdin.write(constants.UPDATE_CMD + '\n');
+    });
+
+    // Update devbox
+    ssh.stdout.on('data', (data) => {
+        let message = data.toString();
+
+        if (_.contains(message, 'sicksync@')) {
+            console.info(project.hostname, text.UPDATE_SUCCESS);
+
+            return ssh.kill('SIGINT');
+        }
+
+        if (_.contains(message, 'ERR!')) {
+            console.info(project.hostname, text.UPDATE_FAIL);
+
+            return ssh.kill('SIGINT');
+        }
+
+        updateRemoteOnce(ssh.stdin);
+    });
+}
+
+function updateLocal () {
+    exec(constants.UPDATE_CMD,  (error, stdout, stderr) => {
+        if (!!error || _.contains(stderr, 'ERR!')) {
+            return console.info(hostname, text.UPDATE_FAIL, (error || stderr));
+        }
+
+        console.info(hostname, text.UPDATE_SUCCESS);
+    });
+}
+
+function update (config, opts) {
+    if (opts.check) {
+        return getLatestVersion(function(err, version) {
+            if (err) return;
+            console.info('Latest Version:', version);
+            console.info('Current Version:', packageJson.version);
         });
-
-        // Update devbox
-        ssh.stdout.on('data', (data) => {
-            let message = data.toString();
-
-            if (_.contains(message, 'sicksync@')) {
-                console.info(project.hostname, text.UPDATE_SUCCESS);
-
-                return ssh.kill('SIGINT');
-            }
-
-            if (_.contains(message, 'ERR!')) {
-                console.info(project.hostname, text.UPDATE_FAIL);
-
-                return ssh.kill('SIGINT');
-            }
-
-            updateRemoteOnce(ssh.stdin);
-        });
-    },
-    updateLocal () {
-        exec(constants.UPDATE_CMD,  (error, stdout, stderr) => {
-            if (!!error || _.contains(stderr, 'ERR!')) {
-                return console.info(hostname, text.UPDATE_FAIL, (error || stderr));
-            }
-
-            console.info(hostname, text.UPDATE_SUCCESS);
-        });
-    },
-    update (config, opts) {
-        if (opts.check) {
-            return this.getLatestVersion(function(err, version) {
-                if (err) return;
-                console.info('Latest Version:', version);
-                console.info('Current Version:', packageJson.version);
-            });
-        }
-
-        if (opts.migrateConfig) {
-            return this.migrateConfig(config);
-        }
-
-        _.each(config.projects, this.updateRemote);
-
-        this.updateLocal();
-    },
-    notify () {
-        if (updateInfo.version !== packageJson.version) {
-            return console.info(
-                text.UPDATE_AVAILABLE, '\n',
-                'Current version:', packageJson.version, '\n',
-                'Latest version:', updateInfo.version);
-        }
-    },
-    check () {
-        if (now - updateInfo.lastChecked >= constants.UPDATE_INTERVAL) {
-            this.getLatestVersion(function (err, version) {
-                if (err) return;
-                fs.writeFileSync(
-                    util.getUpdatePath(), 
-                    JSON.stringify({
-                        version: version,
-                        lastChecked: now
-                    })
-                );
-            });
-        }
-    },
-    migrate1to2(config) {
-        if (_.isEmpty(config)) {
-            let configLocation = untildify(constants.CONFIG_FILE_V1);
-            config = require(configLocation);
-            fs.delete(configLocation);
-        }
-
-        config.project = path.basename(config.sourceLocation);
-
-        return {
-            version: packageJson.version,
-            debug: config.debug,
-            retryOnDisconnect: config.retryOnDisconnect,
-            projects: [
-                _.chain(config)
-                    .omit('debug', 'retryOnDisconnect')
-                    .mapKeys(function(value, key) {
-                        if (key === 'userName') return 'username';
-                        return key;
-                    })
-                    .value()
-            ]
-        };
-    },
-    migrateConfig(config) {
-        let configVersion = config.version ? +configVersion.replace('.', '') : null;
-
-        if (!configVersion) {
-            util.writeConfig(this.migrate1to2(config));
-        }
     }
+
+    if (opts.migrateConfig) {
+        return migrateConfig(config);
+    }
+
+    _.each(config.projects, updateRemote);
+
+    updateLocal();
+}
+
+function notify () {
+    if (updateInfo.version !== packageJson.version) {
+        return console.info(
+            text.UPDATE_AVAILABLE, '\n',
+            'Current version:', packageJson.version, '\n',
+            'Latest version:', updateInfo.version);
+    }
+}
+
+function check () {
+    if (now - updateInfo.lastChecked >= constants.UPDATE_INTERVAL) {
+        getLatestVersion(function (err, version) {
+            if (err) return;
+            fs.writeFileSync(
+                util.getUpdatePath(), 
+                JSON.stringify({
+                    version: version,
+                    lastChecked: now
+                })
+            );
+        });
+    }
+}
+
+function migrate1to2(config) {
+    if (_.isEmpty(config)) {
+        let configLocation = untildify(constants.CONFIG_FILE_V1);
+        config = require(configLocation);
+        fs.delete(configLocation);
+    }
+
+    config.project = path.basename(config.sourceLocation);
+
+    return {
+        version: packageJson.version,
+        debug: config.debug,
+        retryOnDisconnect: config.retryOnDisconnect,
+        projects: [
+            _.chain(config)
+                .omit('debug', 'retryOnDisconnect')
+                .mapKeys(function(value, key) {
+                    if (key === 'userName') return 'username';
+                    return key;
+                })
+                .value()
+        ]
+    };
+}
+
+function migrateConfig(config) {
+    let configVersion = config.version ? +configVersion.replace('.', '') : null;
+
+    if (!configVersion) {
+        util.writeConfig(migrate1to2(config));
+    }
+}
+
+export default {
+    migrate1to2,
+    migrateConfig,
+    update,
+    notify,
+    check,
+    updateLocal,
+    updateRemote,
+    getLatestVersion
 };
