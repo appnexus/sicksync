551e61db3e95ff58549b85c94f1a65c99752f08e
Joel Griffith <jgriffith@appnexus.com>
2015-09-16 13:45:52 -0700

    Updating some packages, bigsync changes

--- conf/constants.js
+++ conf/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
     UPDATE_CMD: 'npm i -g sicksync',
     UPDATE_INTERVAL: 1000 * 60 * 60 * 24,
-    NUM_FILES_FOR_RSYNC: 5,
-    FILE_CHANGE_COOLDOWN_MS: 10,
+    NUM_FILES_FOR_RSYNC: 3,
+    FILE_CHANGE_COOLDOWN_MS: 50,
     CRYPT_ALGO: 'aes-128-ctr',
     CONFIG_FILE: 'config.json',
     CONFIG_FILE_V1: '~/.sicksync-config.json',
